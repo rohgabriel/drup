5bb07bcd161ed8daa082fee3aef5ca0dc4ea64b5
rohgabriel <rohgabriel@gmail.com>
2014-10-30 17:52:12 -0600

    jueves 30 oct

--- sites/all/themes/roh/templates/page.tpl.php
+++ sites/all/themes/roh/templates/page.tpl.php
@@ -75,19 +75,23 @@
 ?>
 
   <?php print render($page['imagen_cabecera']); ?>
-  
-<header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
-  <div class="container">
-    <div class="navbar-header">
-      <?php if ($logo): ?>
+  <?php print render($page['header']); ?>
+   <?php print render($page['logo']); ?>
+  <?php /* if ($logo): ?>
       <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
         <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
       </a>
-      <?php endif; ?>
+      <?php endif; */?>
 
-      <?php if (!empty($site_name)): ?>
+
+<header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
+  <div class="container">
+    <div class="navbar-header">
+      
+
+      <?php /*if (!empty($site_name)): ?>
       <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
-      <?php endif; ?>
+      <?php endif;*/ ?>
 
       <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
@@ -123,16 +127,16 @@
       <p class="lead"><?php print $site_slogan; ?></p>
     <?php endif; ?>
 
-    <?php print render($page['header']); ?>
+ 
   </header> <!-- /#page-header -->
 
   <div class="row">
 
-    <?php if (!empty($page['sidebar_first'])): ?>
+    <?php /* if (!empty($page['sidebar_first'])): ?>
       <aside class="col-sm-3" role="complementary">
         <?php print render($page['sidebar_first']); ?>
       </aside>  <!-- /#sidebar-first -->
-    <?php endif; ?>
+    <?php endif; */?>
 
     <section<?php print $content_column_class; ?>>
       <?php if (!empty($page['highlighted'])): ?>
@@ -141,11 +145,11 @@
       <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
-      <?php if (!empty($title)): ?>
+      <?php /* if (!empty($title)): ?>
         <h1 class="page-header"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
+      <?php endif; */?>
+      <?php /* print render($title_suffix); ?>
+      <?php print $messages;*/ ?>
       <?php if (!empty($tabs)): ?>
         <?php print render($tabs); ?>
       <?php endif; ?>
